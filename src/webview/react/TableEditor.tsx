2fd0d326923da8589e01c138eb9917f3f5a1e023
yuya296 <yuya296@gmail.com>
2025-05-02 21:27:34 +0900

    chore: react対応

---
+++ src/webview/react/TableEditor.tsx
@@ -0,0 +1,36 @@
+// src/webview/react/TableEditor.tsx
+import React, { useState } from "react";
+
+export const TableEditor: React.FC = () => {
+  const [tableData, setTableData] = useState([
+    ["Header1", "Header2"],
+    ["Cell1", "Cell2"],
+  ]);
+
+  return (
+    <div>
+      <table>
+        <tbody>
+          {tableData.map((row, rowIdx) => (
+            <tr key={rowIdx}>
+              {row.map((cell, colIdx) => (
+                <td key={colIdx} contentEditable suppressContentEditableWarning>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ marginTop: 8 }}>
+        <button>行追加</button>
+        <button>列追加</button>
+        <button>行削除</button>
+        <button>列削除</button>
+        <button>元に戻す</button>
+        <button>やり直し</button>
+        <button>保存</button>
+      </div>
+    </div>
+  );
+};
